e357aa032c93bd9f1c3cf54deae99ca5c1a59a11
Sahil Suri <sahil.suri@rokt.com>
2022-03-04 13:32:42 +1100

    ECOM-220 callback from native to flutter

--- android/src/main/kotlin/com/rokt/rokt_sdk/MethodCallHandlerImpl.kt
+++ android/src/main/kotlin/com/rokt/rokt_sdk/MethodCallHandlerImpl.kt
@@ -8,9 +8,9 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 
 class MethodCallHandlerImpl(private val messenger: BinaryMessenger) :
-    MethodChannel.MethodCallHandler, Rokt.RoktCallback {
+    MethodChannel.MethodCallHandler {
     private var channel: MethodChannel? = null
-    lateinit var activity: Activity
+    private lateinit var activity: Activity
 
     override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
         when (call.method) {
@@ -57,8 +57,34 @@ class MethodCallHandlerImpl(private val messenger: BinaryMessenger) :
     private fun execute(call: MethodCall, result: MethodChannel.Result) {
         val viewName = call.argument<String>("viewName").orEmpty()
         val attributes = call.argument<HashMap<String, String>>("attributes").orEmpty()
-        Log.d(TAG, "execute $viewName $attributes")
-        Rokt.execute(viewName, attributes, this, mapOf())
+        val callBackId = call.argument<Int>("callbackId") ?: 0
+        val map: MutableMap<String, Any> = mutableMapOf()
+        map["id"] = callBackId
+        Log.d(TAG, "execute $viewName $attributes ${map["id"]}")
+        Rokt.execute(viewName, attributes, object : Rokt.RoktCallback {
+            override fun onUnload(reason: Rokt.UnloadReasons) {
+                map["args"] = "unload"
+                channel?.invokeMethod("callListener", map)
+                Log.d(TAG, "onUnLoad")
+            }
+
+            override fun onLoad() {
+                map["args"] = "load"
+                channel?.invokeMethod("callListener", map)
+                Log.d(TAG, "loaded")
+            }
+
+            override fun onShouldHideLoadingIndicator() {
+                map["args"] = "onShouldHideLoadingIndicator"
+                channel?.invokeMethod("callListener", map)
+                Log.d(TAG, "onShouldHideLoadingIndicator")
+            }
+            override fun onShouldShowLoadingIndicator() {
+                map["args"] = "onShouldShowLoadingIndicator"
+                channel?.invokeMethod("callListener", map)
+                Log.d(TAG, "onShouldShowLoadingIndicator")
+            }
+        }  , mapOf())
         result.success("Executed")
     }
 
@@ -67,19 +93,5 @@ class MethodCallHandlerImpl(private val messenger: BinaryMessenger) :
         private const val INIT_METHOD = "initialize"
         private const val EXECUTE_METHOD = "execute"
         private const val TAG = "Rokt_MethodCallHandler"
-    }
-
-    override fun onLoad() {
-        Log.d(TAG, "onLoad")
-    }
-
-    override fun onShouldHideLoadingIndicator() {
-    }
-
-    override fun onShouldShowLoadingIndicator() {
-    }
-
-    override fun onUnload(reason: Rokt.UnloadReasons) {
-        Log.d(TAG, "onUnLoad")
     }
 }
