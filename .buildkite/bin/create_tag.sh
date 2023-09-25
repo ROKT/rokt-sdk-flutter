e74ed5978c42e09d93064c48c40b881e95415333
Lewis Krishnamurti <lewis.raj.krishnamurti@rokt.com>
2023-09-25 17:08:35 +1000

    Add changelog to tag

--- .buildkite/bin/create_tag.sh
+++ .buildkite/bin/create_tag.sh
@@ -15,6 +15,7 @@ git tag -d "v$1" || true
 git add ./android/build.gradle
 git add ./ios/rokt_sdk.podspec
 git add pubspec.yaml
+git add CHANGELOG.md
 git commit -m "v$1"
 git tag -a "v$1" -m "Automated release v$1"
 git push origin "v$1"
