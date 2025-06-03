beafc5b1bacaa76e2b1eca84c5faa3e0e140d0c0
James Newman <james.newman@rokt.com>
2025-06-03 15:08:53 +1000

    Trunk fixes

--- pub_login.sh
+++ pub_login.sh
@@ -4,30 +4,30 @@
 
 # Checking whether the secrets are available as environment
 # variables or not.
-if [ -z "${PUB_DEV_PUBLISH_ACCESS_TOKEN}" ]; then
+if [[ -z ${PUB_DEV_PUBLISH_ACCESS_TOKEN} ]]; then
 	echo "Missing PUB_DEV_PUBLISH_ACCESS_TOKEN environment variable"
 	exit 1
 fi
 
-if [ -z "${PUB_DEV_PUBLISH_REFRESH_TOKEN}" ]; then
+if [[ -z ${PUB_DEV_PUBLISH_REFRESH_TOKEN} ]]; then
 	echo "Missing PUB_DEV_PUBLISH_REFRESH_TOKEN environment variable"
 	exit 1
 fi
 
-if [ -z "${PUB_DEV_PUBLISH_TOKEN_ENDPOINT}" ]; then
+if [[ -z ${PUB_DEV_PUBLISH_TOKEN_ENDPOINT} ]]; then
 	echo "Missing PUB_DEV_PUBLISH_TOKEN_ENDPOINT environment variable"
 	exit 1
 fi
 
-if [ -z "${PUB_DEV_PUBLISH_EXPIRATION}" ]; then
+if [[ -z ${PUB_DEV_PUBLISH_EXPIRATION} ]]; then
 	echo "Missing PUB_DEV_PUBLISH_EXPIRATION environment variable"
 	exit 1
 fi
 
 # Create credentials.json file.
 echo "Copy credentials"
-mkdir -p $HOME/.config/dart/
-cat <<EOF >$HOME/.config/dart/pub-credentials.json
+mkdir -p "${HOME}"/.config/dart/
+cat <<EOF >"${HOME}"/.config/dart/pub-credentials.json
 {
   "accessToken":"${PUB_DEV_PUBLISH_ACCESS_TOKEN}",
   "refreshToken":"${PUB_DEV_PUBLISH_REFRESH_TOKEN}",
